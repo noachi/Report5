800fd8957cbdf6a9db442affefba7d84c6843833
Noa Teruya <e215715@ie.u-ryukyu.ac.jp>
2021-12-20 23:44:24 +0900

    [add]例外処理を追加

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 class Main{
     public static void main(String[]args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println("NullPointException例外をcatchしました");
+            System.out.println("ーースタックトレース（ここから）ーー");
+            e.printStackTrace();
+            System.out.println("ーースタックトレースーー（ここまで）ーー");
+        }
     }
 }
